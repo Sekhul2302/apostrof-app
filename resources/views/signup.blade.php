58b95ba85c49d9be6b05d27f30f0b40decc7b4a0
SEKHULAMIN\sekhu <sekhulamin@gmail.com>
2023-07-05 23:58:00 +0700

    add view sign in, login, home

---
+++ resources/views/signup.blade.php
@@ -0,0 +1,138 @@
+<!--
+=========================================================
+* Material Dashboard 2 PRO - v3.0.6
+=========================================================
+
+* Product Page:  https://www.creative-tim.com/product/material-dashboard-pro
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+-->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="apple-touch-icon" sizes="76x76" href="../../../assets/img/apple-icon.png">
+    <link rel="icon" type="image/png" href="../../../assets/img/favicon.png">
+    <title>
+        Material Dashboard 2 PRO by Creative Tim
+    </title>
+    <!--     Fonts and icons     -->
+    <link rel="stylesheet" type="text/css"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
+    <!-- Nucleo Icons -->
+    <link href="../../../assets/css/nucleo-icons.css" rel="stylesheet" />
+    <link href="../../../assets/css/nucleo-svg.css" rel="stylesheet" />
+    <!-- Font Awesome Icons -->
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+    <!-- Material Icons -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
+    <!-- CSS Files -->
+    <link id="pagestyle" href="../../../assets/css/material-dashboard.css?v=3.0.6" rel="stylesheet" />
+</head>
+
+<body class="">
+    <!-- Navbar -->
+    <nav
+        class="navbar navbar-expand-lg position-absolute top-0 z-index-3 w-100 shadow-none my-3 navbar-transparent mt-4">
+
+    </nav>
+    <!-- End Navbar -->
+    <main class="main-content  mt-0">
+        <div class="page-header align-items-start min-vh-100"
+            style="background-image: url('https://images.unsplash.com/photo-1448375240586-882707db888b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1650&q=80');">
+            <span class="mask bg-gradient-dark opacity-6"></span>
+            <div class="container my-auto">
+                <div class="row">
+                    <div class="col-lg-4 col-md-8 mx-auto">
+                        <div class="card z-index-0">
+                            <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+                                <div class="bg-gradient-success shadow-success border-radius-lg py-3 pe-1">
+                                    <h4 class="text-white font-weight-bolder text-center mt-2 mb-0">Register Apostrof.id
+                                    </h4>
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                <form role="form">
+                                    <div class="input-group input-group-dynamic mb-3">
+                                        <label class="form-label">Name</label>
+                                        <input type="text" class="form-control" aria-label="Name">
+                                    </div>
+                                    <div class="input-group input-group-dynamic mb-3">
+                                        <label class="form-label">Email</label>
+                                        <input type="email" class="form-control" aria-label="Email">
+                                    </div>
+                                    <div class="input-group input-group-dynamic mb-3">
+                                        <label class="form-label">Password</label>
+                                        <input type="password" class="form-control" aria-label="Password">
+                                    </div>
+                                    <div class="form-check text-start ps-0">
+                                        <input class="form-check-input bg-dark border-dark" type="checkbox"
+                                            value="" id="flexCheckDefault" checked>
+                                        <label class="form-check-label" for="flexCheckDefault">
+                                            I agree the <a href="javascript:;"
+                                                class="text-dark font-weight-bolder">Terms and Conditions</a>
+                                        </label>
+                                    </div>
+                                    <div class="text-center">
+                                        <button type="button" class="btn bg-gradient-dark w-100 my-4 mb-2">Sign
+                                            up</button>
+                                    </div>
+                                    <p class="text-sm mt-3 mb-0">Already have an account? <a href="/signin"
+                                            class="text-dark font-weight-bolder">Sign
+                                            In</a></p>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <footer class="footer position-absolute bottom-2 py-2 w-100">
+                <div class="container">
+                    <div class="row align-items-center justify-content-lg-between">
+                        <div class="col-12 col-md-6 my-auto">
+                            <div class="copyright text-center text-sm text-white text-lg-start">
+                                ©
+                                <script>
+                                    document.write(new Date().getFullYear())
+                                </script>,
+                                made with <i class="fa fa-heart" aria-hidden="true"></i> by
+                                <a href="https://www.creative-tim.com" class="font-weight-bold text-white"
+                                    target="_blank">Creative Tim</a>
+                                for a better web.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </main>
+    <!--   Core JS Files   -->
+    <script src="../../../assets/js/core/popper.min.js"></script>
+    <script src="../../../assets/js/core/bootstrap.min.js"></script>
+    <script src="../../../assets/js/plugins/perfect-scrollbar.min.js"></script>
+    <script src="../../../assets/js/plugins/smooth-scrollbar.min.js"></script>
+    <!-- Kanban scripts -->
+    <script src="../../../assets/js/plugins/dragula/dragula.min.js"></script>
+    <script src="../../../assets/js/plugins/jkanban/jkanban.js"></script>
+    <script>
+        var win = navigator.platform.indexOf('Win') > -1;
+        if (win && document.querySelector('#sidenav-scrollbar')) {
+            var options = {
+                damping: '0.5'
+            }
+            Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+        }
+    </script>
+    <!-- Github buttons -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+    <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
+    <script src="../../../assets/js/material-dashboard.min.js?v=3.0.6"></script>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/signin', function () {
+    return view('signin');
+});
+
+Route::get('/signup', function () {
+    return view('signup');
+});
+
+Route::get('/profile', function () {
+    return view('profile');
 });
